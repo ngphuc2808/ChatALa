1a2b0c451689a9ee2ac10f2f52c3e0e7889beb57
HungTran2101 <89181217+HungTran2101@users.noreply.github.com>
2022-09-13 00:39:58 +0700

    update project structure

---
+++ pages/about.tsx
@@ -0,0 +1,11 @@
+import Title from "../src/components/About/Title";
+
+const about = () => {
+  return (
+    <>
+      <Title />
+    </>
+  );
+};
+
+export default about;
---
+++ src/components/About/Title/index.tsx
@@ -0,0 +1,11 @@
+import * as S from "./Title.styled";
+
+const Title = () => {
+  return (
+    <S.Title>
+      Title of About
+    </S.Title>
+  );
+};
+
+export default Title;
