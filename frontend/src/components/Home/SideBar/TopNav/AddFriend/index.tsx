6584fc17fffc3a4357a55690e8dd5c2d428ba662
HungTran2101 <89181217+HungTran2101@users.noreply.github.com>
2022-09-29 10:42:31 +0700

    addfriend modal

--- frontend/src/components/Home/SideBar/TopNav/AddFriend/index.tsx
+++ frontend/src/components/Home/SideBar/TopNav/AddFriend/index.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+import { NotiListArray } from "../../../../../utils/dataConfig";
 import * as S from "./AddFriend.styled";
 
 interface IAddFriend {
@@ -9,7 +11,24 @@ const AddFriend = ({ setToggleAddFriend }: IAddFriend) => {
     <S.AddFriendModal>
       <S.AddFriendOverlay onClick={() => setToggleAddFriend(false)} />
       <S.AddFriendBody>
-        asdasd
+        <S.AddFriendSearch>
+          <S.AddFriendSearchIcon />
+          <S.AddFriendSearchInput placeholder="Search with name or phone number..." />
+        </S.AddFriendSearch>
+        {NotiListArray.map((data, index) => (
+          <S.AddFriendItem key={index}>
+            <S.AddFriendInfo>
+              <S.AddFriendAvatar>
+                <Image src={data.avatar} alt="avatar" />
+              </S.AddFriendAvatar>
+              <S.AddFriendNameWrapper>
+                <S.AddFriendName>{data.name}</S.AddFriendName>
+                <S.AddFriendNumFriend>{`${data.numFriends} Friends`}</S.AddFriendNumFriend>
+              </S.AddFriendNameWrapper>
+            </S.AddFriendInfo>
+            <S.AddFriendOption>Add friend</S.AddFriendOption>
+          </S.AddFriendItem>
+        ))}
       </S.AddFriendBody>
     </S.AddFriendModal>
   );
