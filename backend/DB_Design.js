f8602a522947e5b0a47c09499bc7cffb3a47e2c0
HungTran2101 <89181217+HungTran2101@users.noreply.github.com>
2022-10-01 20:28:50 +0700

    update

---
+++ backend/DB_Design.js
@@ -0,0 +1,19 @@
+const Room = {
+  _id,
+  users: [uid1, uid2]
+}
+
+const Message = {
+  roomId,
+  senderId,
+  msg,
+}
+
+const User = {
+  _id,
+  avatar,
+  banner,
+  name,
+  gender,
+  dob
+}
