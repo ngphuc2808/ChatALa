ba13ea15905f06b9940c0319e4f6b3455da63039
Trần Hữu Thanh <88789960+IT0505@users.noreply.github.com>
2022-10-22 22:43:43 +0700

    Update fix blocked by CORS policy

--- backend/server.js
+++ backend/server.js
@@ -1,31 +1,38 @@
-require("colors");
-const express = require("express");
-const dotenv = require("dotenv");
+require('colors');
+const express = require('express');
+const dotenv = require('dotenv');
 const cookieParser = require('cookie-parser');
-const { connectDB } = require("./config/db");
-const userRoutes = require("./routes/userRoutes");
-const errorMiddleware = require("./middlewares/errors");
+const { connectDB } = require('./config/db');
+const userRoutes = require('./routes/userRoutes');
+const errorMiddleware = require('./middlewares/errors');
+const cors = require('cors');
 
 const PORT = process.env.PORT || 5000;
 
-process.on("uncaughtException", (err) => {
-  console.log("ERROR: " + err.stack);
-  console.log("Shutting down the server due to Uncaught Exception");
+const corsOptions = {
+  origin: 'http://localhost:3000',
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
+process.on('uncaughtException', (err) => {
+  console.log('ERROR: ' + err.stack);
+  console.log('Shutting down the server due to Uncaught Exception');
   process.exit(1);
 });
 
 dotenv.config();
 
 const app = express();
+app.use(cors(corsOptions));
 app.use(express.json()); //allow accept json data
 app.use(cookieParser(process.env.COOKIE_SECRET));
 
-app.get("/", (req, res) => {
-  res.send("server is ready!");
+app.get('/', (req, res) => {
+  res.send('server is ready!');
 });
 
 //route
-app.use("/api/user", userRoutes);
+app.use('/api/user', userRoutes);
 
 //middleware
 app.use(errorMiddleware); //handle error
@@ -34,6 +41,9 @@ app.use(errorMiddleware); //handle error
 connectDB().then(() =>
   app.listen(
     PORT,
-    console.log(`Server started on http://localhost:${PORT} in ${process.env.NODE_ENV} mode`.yellow.bold)
+    console.log(
+      `Server started on http://localhost:${PORT} in ${process.env.NODE_ENV} mode`
+        .yellow.bold
+    )
   )
 );
