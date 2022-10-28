2d7bfa642cd197bcbb5d8cec584673cdce96ccb7
ngphuc2808 <ngphuc.2808@gmail.com>
2022-10-28 23:26:38 +0700

    Update Backend FindUser

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 require("colors");
 const express = require("express");
+const cors = require('cors');
 const dotenv = require("dotenv");
 const cookieParser = require('cookie-parser');
 const { connectDB } = require("./config/db");
@@ -7,6 +8,12 @@ const userRoutes = require("./routes/userRoutes");
 const errorMiddleware = require("./middlewares/errors");
 
 const PORT = process.env.PORT || 5000;
+
+const corsOptions = {
+  origin:'http://localhost:3000', 
+  credentials:true,            //access-control-allow-credentials:true
+  optionSuccessStatus:200
+}
 
 process.on("uncaughtException", (err) => {
   console.log("ERROR: " + err.stack);
@@ -17,6 +24,7 @@ process.on("uncaughtException", (err) => {
 dotenv.config();
 
 const app = express();
+app.use(cors(corsOptions));
 app.use(express.json()); //allow accept json data
 app.use(cookieParser(process.env.COOKIE_SECRET));
 
