c4308253dd741214d7532bb384cb31cbbc55fce7
HungTran2101 <89181217+HungTran2101@users.noreply.github.com>
2022-11-06 17:33:17 +0700

    fix css

--- frontend/src/components/Home/ChatArea/ChatMsg/ChatMsg.styled.ts
+++ frontend/src/components/Home/ChatArea/ChatMsg/ChatMsg.styled.ts
@@ -8,6 +8,7 @@ export const ChatMsg = styled.div`
 
 export const ChatMsgText = styled.div`
   ${tw`relative px-3 py-2 max-w-[70%]`}
+  width: fit-content;
 `;
 
 export const ChatMsgTextTail = styled.div`
@@ -82,10 +83,11 @@ export const ChatMsgFile = styled.div`
 
 export const ChatMsgUnSend = styled.div`
   ${tw`border-2 py-2 px-2.5`}
+  width: fit-content;
 `;
 
 export const ChatMsgWrapper = styled.div`
-  ${tw`flex flex-col`}
+  ${tw`flex flex-col flex-grow`}
 `;
 
 export const ChatMsgLeft = styled(ChatMsg)<{ position: string }>`
