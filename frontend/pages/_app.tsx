2f5d3a87e6a81a535d928c92912a2a86d52db543
TheHien102 <88789960+IT0505@users.noreply.github.com>
2022-12-21 14:53:27 +0700

    Update Loading js

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -3,19 +3,25 @@ import type { AppProps } from "next/app";
 import { Provider as ReduxProvider } from "react-redux";
 import Head from "next/head";
 import { wrapper } from "../src/features/redux/store";
-import { FC } from "react";
+import { FC, useEffect, useState } from "react";
 import { SocketProvider } from "../src/contexts/socket";
 
 const MyApp: FC<AppProps> = ({ Component, ...rest }) => {
   const { store, props } = wrapper.useWrappedStore(rest);
   const { pageProps } = props;
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setLoading(false);
+  }, []);
+
   return (
     <SocketProvider>
       <ReduxProvider store={store}>
         <Head>
           <title>Chatala</title>
         </Head>
-        <Component {...pageProps} />
+        {!loading ? <Component {...pageProps} /> : "Loading..."}
       </ReduxProvider>
     </SocketProvider>
   );
